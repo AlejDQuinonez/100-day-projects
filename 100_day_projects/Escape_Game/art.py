40fb5da96808d2f5b14314949a489e6ec129fcd3
AlejDQuinonez <132519421+AlejDQuinonez@users.noreply.github.com>
2024-03-25 21:31:48 -0400

    Add files via upload

---
+++ 100_day_projects/Escape_Game/art.py
@@ -0,0 +1,8 @@
+mad_face     = "😡"
+ghost        = "👻"
+devil        = "👿"
+happy_face   = "😄"
+placeHolder  = "⬛"
+goal         = "🟩"
+dead         = "💀"
+enemies_list = [mad_face, ghost, devil]
---
+++ 100_day_projects/Escape_Game/main.py
@@ -0,0 +1,89 @@
+from art import *
+import random
+import sys
+
+print()
+startGame = input("Would you like to play Escape?\n").lower()
+
+#Creates the size of the Grid
+width = 10
+height = 10
+matrix = [[placeHolder for x in range(width)] for y in range(height)]
+
+#Player and Goal starting Positions
+player_position_x = 0
+player_position_y = 0
+matrix[player_position_y][player_position_x] = happy_face
+
+goal_position_x = 9
+goal_position_y = 9
+matrix[goal_position_x][goal_position_y] = goal
+
+#Randomizes enemy positions (for three enemies)
+enemy_position_x = random.randrange(0,10)
+enemy_position_y = random.randrange(0,10)
+enemy_position_z = random.randrange(0,10)
+matrix[enemy_position_y][enemy_position_z] = random.choice(enemies_list)
+
+#Functions that move the player
+def right(move_right):
+    return 1 + move_right
+
+def left(move_left):
+    return move_left - 1
+
+def up(move_up):
+    return move_up - 1
+
+def down(move_down):
+    return 1 + move_down
+
+if startGame == "exit":
+    print()
+    print("Good-Bye.")
+    sys,exit()
+
+while startGame == "y":
+    print()
+    #Prints Grid
+    print(*matrix, sep = "\n")
+    print()
+
+    #Erases player's previous position
+    matrix[player_position_y].pop(player_position_x)
+    matrix[player_position_y].insert(player_position_x, placeHolder)
+
+    #Player's decision on where to move
+    player_movement = input("Where would you like to move?\n").lower()
+    if player_movement == "right":
+        player_position_x = right(player_position_x)
+    elif player_movement == "left":
+        player_position_x = left(player_position_x)
+    elif player_movement == "up":
+        player_position_y = up(player_position_y)
+    elif player_movement == "down":
+        player_position_y = down(player_position_y)
+    elif player_movement == "exit":
+        print()
+        print("Quitter.")
+        break
+
+    #If player matches same position as enemy, they lose
+    if player_position_x == enemy_position_y and player_position_y == enemy_position_z:
+        matrix[player_position_x][player_position_y] = dead
+        print()
+        print("YOU DIED.")
+        print()
+        print(*matrix, sep = "\n")
+        print()
+        break
+
+    #Changes enemy's and player's positions
+    matrix[enemy_position_y][enemy_position_z] = random.choice(enemies_list)
+    matrix[player_position_y][player_position_x] = happy_face
+    matrix[goal_position_x][goal_position_y] = goal
+
+    print()
+    print(*matrix, sep = "\n")
+    print()
+    
